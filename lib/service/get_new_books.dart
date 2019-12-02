994f347ad0fb5d7b20f275a26a6656172dad335f
Nathan Kimutai <nathanpykimutai@gmail.com>
2019-12-02 18:23:26 +0300

    Implemented the service for getting new books from the api

--- lib/service/get_new_books.dart
+++ lib/service/get_new_books.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/rendering.dart';
 import 'package:http/http.dart' as http;
+import 
+
+void getData() async {
+  final response = await http.get("https://api.itbook.store/1.0/new");
+  debugPrint(response.body);
+}
