285f3438f404112de03fceb8e09ce390b41e0047
Nathan Kimutai <nathanpykimutai@gmail.com>
2019-12-03 14:59:48 +0300

    Adding the custom shape clipper

---
+++ lib/utils/top_clipper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TopClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = new Path();
+
+    return null;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
